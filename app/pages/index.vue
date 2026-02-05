242b4efb7fbbf25acff76e7b6c64295e3c4e8846
abbjetmus <jeton.mustini@gmail.com>
2026-02-05 10:08:22 +0100

    first commit

---
+++ app/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <v-row justify="center" class="mt-10">
+      <v-col cols="12" md="8" lg="6">
+        <v-card class="pa-6" elevation="2">
+          <v-card-title class="text-h4 text-center">
+            Nuxt Vuetify Template
+          </v-card-title>
+          <v-card-text class="text-body-1 text-center mt-4">
+            This is a starter template with Nuxt 4 and Vuetify 3.
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <example-comp />
+  </v-container>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: true,
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['vuetify-nuxt-module'],
+  vuetify: {
+    vuetifyOptions: {
+      theme: {
+        defaultTheme: 'light',
+      },
+    },
+  },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'Nuxt Vuetify Template',
+    },
+  },
+})
